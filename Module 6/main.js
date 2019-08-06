1116348eb331c6013a302bb83435dd53c9643e55
Sazuru <sazuru@gmail.com>
2019-08-06 21:08:11 +0300

    added cycle, removed main1

--- Module 6/main.js
+++ Module 6/main.js
@@ -21,3 +21,17 @@ function UserList() {
         return this.users;
     };
 }
+
+var x = new UserList();
+
+while (promptName !== null) {
+    var promptName = prompt('Enter your full name');
+    if (promptName !== null) {
+        x.add(new User({
+            firstName: promptName.trim().split(' ')[0],
+            lastName: promptName.trim().split(' ')[1]
+        }));
+    } else {
+        console.log(x.getAllUsers());
+    }
+}
