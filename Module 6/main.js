5b377a7fc3997e2d9d904c91c49df6a585ab6e53
Sazuru <sazuru@gmail.com>
2019-08-06 21:05:09 +0300

    Added 'UserList' constructor

--- Module 6/main.js
+++ Module 6/main.js
@@ -6,3 +6,18 @@ function User() {
         var date = new Date();
     this.regDate = date.toDateString;
 }
+
+function UserList() {
+    this.users = [];
+    for (var i = 0; i < this.users.length; i++) {
+        if (users[i] == promptName) {
+            return;
+        }
+    }
+    this.add = function (user) {
+        this.users.push(user);
+    };
+    this.getAllUsers = function () {
+        return this.users;
+    };
+}
