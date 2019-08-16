7945ed6e174d15683fe1664fee42fd1a08c5a841
Sazuru <sazuru@gmail.com>
2019-08-16 20:53:32 +0300

    Started new module, added first testing code

--- Module 7/main.js
+++ Module 7/main.js
@@ -1,1 +1,20 @@
 'use strict';
+//Базовая функция-конструктор
+
+function User(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    this.getFullName = function () {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+//Дочернаяя функция-конструктор
+function Manager(firstName, lastName) {
+    User.apply(this, arguments);
+
+    this.sayHello = function () {
+        alert('Здравствуйте');
+    }
+}
