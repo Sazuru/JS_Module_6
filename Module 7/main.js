df2f1146f17456b657557624fdb0272776b6d699
Sazuru <sazuru@gmail.com>
2019-08-20 21:54:36 +0300

    Created the parent constructor

--- Module 7/main.js
+++ Module 7/main.js
@@ -1,24 +1,44 @@
 'use strict';
 //Базовая функция-конструктор
 
-function User(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+function Vehicle(model, year, color, passengers) {
+    this.model = model;
+    this.color = color;
+    this.passengers = passengers;
+    this.engineStarts = false;
 
-    this.getFullName = function () {
-        return this.firstName + ' ' + this.lastName;
+    this.getVehicleName = function () {
+        console.log(this.model);
+    };
+
+    this.startTheEngine = function () {
+        this.engineStarts = true;
+        console.log('Двигатель заведён');
+    }
+
+    this.stopTheEngine = function () {
+        this.engineStarts = false;
+        console.log('Двигатель выключен');
     }
 }
 
-//Дочернаяя функция-конструктор
-function Manager(firstName, lastName) {
-    User.apply(this, arguments);
+//Дочерние функции-конструкторы
+function Car(model, color, passengers) {
+    Vehicle.apply(this, arguments);
 
-    this.sayHello = function () {
-        alert('Здравствуйте');
-    }
 
-    this.changeName = function (name) {
-        this.firstName = name;
-    }
+
 }
+
+function Airplane(model, color, passengers) {
+    Vehicle.apply(this, arguments);
+
+
+}
+
+function Ship(model, color, passengers) {
+    Vehicle.apply(this, arguments);
+
+}
+
+var vehicle = new Vehicle('Cadillac', 1923, 'red', 2);
