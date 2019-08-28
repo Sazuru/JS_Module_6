7c3c510de9861e9dc47ec4dcf44d0f0a6b71efc2
Sazuru <sazuru@gmail.com>
2019-08-28 22:21:29 +0300

    Added solution for additional task

--- Module 8/mainEval.js
+++ Module 8/mainEval.js
@@ -1,14 +1,34 @@
-'use strict';
-let yourCode = prompt('Введите ваш код: ', '2 + 2');
+//let yourCode = prompt('Введите ваш код: ', '2 + 2');
+//
+//function testCode() {
+//    'use strict';
+//    console.log(eval(yourCode));
+//
+//};
+//
+//try {
+//    testCode();
+//} catch (ex) {
+//    console.log('Ошибка в вашем коде: ' + ex);
+//}
+
+//var userCode = document.querySelector('#btn');
+//userCode.addEventListener('click', function () {
+//    eval(text);
+//});
+
+//function evalCode() {
+//    var yourCode = document.querySelector('textarea').value;
+//    eval(yourCode);
+//}
 
 function testCode() {
-    'use strict';
-    console.log(eval(yourCode));
-
-};
-
-try {
-    testCode();
-} catch (ex) {
-    console.log('Ошибка в вашем коде: ' + ex);
+    try {
+        'use strict';
+        var yourCode = document.getElementById('textarea').value;
+        eval(yourCode);
+    } catch (ex) {
+        er.innerHTML = ('Ошибка в вашем коде: ' + ex);
+        document.getElementById('er').style.color = 'red';
+    }
 }
