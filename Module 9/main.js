d911de6b0be0c56795bc8c6cdd8afcb3f7f8f83d
Sazuru <sazuru@gmail.com>
2019-09-02 23:10:57 +0300

    Added second function

--- Module 9/main.js
+++ Module 9/main.js
@@ -4,7 +4,18 @@ var list = document.getElementById('taskList');
 
 function addTask() {
     var task = document.getElementById('newTask').value;
-    var entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(task));
-    list.appendChild(entry);
+    if (task == 0) {
+        console.log('Ничего не введено!');
+    } else {
+        var entry = document.createElement('li');
+        entry.appendChild(document.createTextNode(task));
+        list.appendChild(entry);
+    }
+}
+
+list.onclick = function (e) {
+    if (e.target.tagName != "UL") {
+        e.target.className == "done" ? e.target.className = "" :
+            e.target.className = "done";
+    }
 }
