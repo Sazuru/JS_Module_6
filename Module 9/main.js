4556a26e731b3aeb5bb4ce1460e28bea6a0ff360
Sazuru <sazuru@gmail.com>
2019-09-02 22:42:35 +0300

    First try to solve

--- Module 9/main.js
+++ Module 9/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var list = document.getElementById('taskList');
+
+function addTask() {
+    var task = document.getElementById('newTask').value;
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(task));
+    list.appendChild(entry);
+}
