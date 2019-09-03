389f3f26e6c3db3e5549e64695790635289a500e
Sazuru <sazuru@gmail.com>
2019-09-03 23:11:01 +0300

    Added new solution for task

--- Module 9/main.js
+++ Module 9/main.js
@@ -4,12 +4,22 @@ var list = document.getElementById('taskList');
 
 function addTask() {
     var task = document.getElementById('newTask').value;
+    var lastChild = list.lastElementChild.innerHTML;
     if (task == 0) {
         console.log('Ничего не введено!');
     } else {
-        var entry = document.createElement('li');
-        entry.appendChild(document.createTextNode(task));
-        list.appendChild(entry);
+        if (task == lastChild) {
+            var ask = confirm('Такая задача уже есть, добавить?')
+            if (ask === true) {
+                var entry = document.createElement('li');
+                entry.appendChild(document.createTextNode(task));
+                list.appendChild(entry);
+            }
+        } else {
+            var entry = document.createElement('li');
+            entry.appendChild(document.createTextNode(task));
+            list.appendChild(entry);
+        }
     }
 }
 
