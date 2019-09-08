45788d378b2c05fa437cc1d893823d142cf63756
Sazuru <sazuru@gmail.com>
2019-09-08 20:53:53 +0300

    added contenteditable for p

--- Module 10/main.js
+++ Module 10/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
-function editText() {}
+function editText() {
+    var edit = document.getElementById('editable');
+    if (edit.contentEditable == "true") {
+        edit.contentEditable = "false";
+    } else {
+        edit.contentEditable = "true";
+    }
+}
 
 function saveText() {}
