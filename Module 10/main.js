275a44b0a2c6f9f9044a905e85af64b64010507a
Sazuru <sazuru@gmail.com>
2019-09-10 23:54:33 +0300

    Task done

--- Module 10/main.js
+++ Module 10/main.js
@@ -1,23 +1,43 @@
 'use strict';
 
 var getText = document.getElementById('editable');
+var editButton = document.getElementById('edit');
+var saveButton = document.getElementById('save');
+var returnButton = document.getElementById('return');
 
 function editText() {
-    var editButton = document.getElementById('edit');
-    if (getText.contentEditable === 'true') {
+    if (getText.contentEditable == 'true') {
         getText.contentEditable = 'false';
-        editButton.setAttribute('value', 'Редактировать');
+        editButton.setAttribute('disabled', true);
     } else {
         getText.contentEditable = 'true';
-        editButton.setAttribute('value', 'Не редактировать');
+        editButton.setAttribute('disabled', true)
+        saveButton.removeAttribute('disabled');
+        returnButton.removeAttribute('disabled');
     }
 }
 
 function saveText() {
     sessionStorage.setItem('save', getText.textContent);
+    getText.contentEditable = 'false';
+    editButton.removeAttribute('disabled');
+    saveButton.setAttribute('disabled', true);
+    returnButton.setAttribute('disabled', true);
 }
 
 function returnText() {
     var oldText = sessionStorage.getItem('save', getText.textContent);
     getText.textContent = oldText;
+    getText.contentEditable = 'false';
+    editButton.removeAttribute('disabled');
+    saveButton.setAttribute('disabled', true);
+    returnButton.setAttribute('disabled', true);
+}
+
+window.onload = function () {
+    if (sessionStorage.getItem('save', getText.textContent) != null) {
+        getText.textContent = sessionStorage.getItem('save', getText.textContent)
+    } else {
+        getText.textContent = document.getElementById('editable').textContent
+    }
 }
