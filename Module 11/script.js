8a1fa48a089b51653bfd965eb0880c39bce92272
Sazuru <sazuru@gmail.com>
2019-09-15 23:01:53 +0300

    Added possibility to choose languages for translation

--- Module 11/script.js
+++ Module 11/script.js
@@ -1,11 +1,78 @@
+var API_KEY = 'trnsl.1.1.20190915T104025Z.3e27a676c53a5add.b9cb4fd9ad1c22453b59a804d154f15ad55bedc2';
+
+window.onload = function () {
+    var urlLangs = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs';
+    urlLangs += '?key=' + API_KEY;
+    urlLangs += '&ui=ru';
+
+    var dropdown1 = document.getElementById('originLanguage');
+    dropdown1.length = 0;
+    var defaultOption1 = document.createElement('option');
+    defaultOption1.text = 'Язык исходного текста';
+    dropdown1.add(defaultOption1);
+    dropdown1.selectedIndex = 0;
+
+    var dropdown2 = document.getElementById('translationLanguage');
+    dropdown2.length = 0;
+    var defaultOption2 = document.createElement('option');
+    defaultOption2.text = 'Язык перевода';
+    dropdown2.add(defaultOption2);
+    dropdown2.selectedIndex = 0;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', urlLangs, true);
+    request.onload = function () {
+        if (request.status === 200) {
+            var data = JSON.parse(request.response);
+            var option;
+            var langs = data.langs;
+            for (var prop in langs) {
+                option = document.createElement('option');
+                option.text = langs[prop];
+                option.value = prop;
+                dropdown1.add(option);
+            };
+            for (var prop in langs) {
+                option = document.createElement('option');
+                option.text = langs[prop];
+                option.value = prop;
+                dropdown2.add(option);
+            };
+        } else {
+            // Reached the server, but it returned an error
+        }
+
+    }
+    request.onerror = function () {
+        console.error('An error occurred fetching the JSON from ' + url);
+    };
+    request.send();
+    //    var request = new XMLHttpRequest();
+    //    request.open('GET', urlLangs, true);
+    //    request.onload = function () {
+    //        if (request.status === 200) {
+    //            var data = JSON.parse(request.response);
+    //            var option;
+    //            var langs = data.langs;
+    //            for (var prop in langs) {
+    //                option = document.createElement('option');
+    //                option.text = langs[prop];
+    //                option.value = prop;
+    //                dropdown2.add(option);
+    //            };
+    //        } else {
+    //            // Reached the server, but it returned an error
+    //        }
+    //
+    //    }
+    //    request.onerror = function () {
+    //        console.error('An error occurred fetching the JSON from ' + url);
+    //    };
+    //    request.send();
+};
+
 function trans() {
-
-    // Создаем объект XMLHttpRequest, при помощи которого будем отправлять запрос
     var req = new XMLHttpRequest();
-
-    // Сохраняем ключ API, полученный со страницы https://tech.yandex.ru/keys/get/?service=trnsl
-    // (с примером ниже работать не будет, нужно получить и вставить свой!)
-    var API_KEY = 'trnsl.1.1.20190915T104025Z.3e27a676c53a5add.b9cb4fd9ad1c22453b59a804d154f15ad55bedc2';
 
     // Сохраняем адрес API
     var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
@@ -13,14 +80,15 @@ function trans() {
     var word = document.getElementById("word");
 
     var text = '&text=' + word.value;
+    var dropdown1 = document.getElementById('originLanguage');
+    var origin = dropdown1.value;
+    var dropdown2 = document.getElementById('translationLanguage');
+    var translation = dropdown2.value;
 
     // Формируем полный адрес запроса:
     url += '?key=' + API_KEY; // добавляем к запросу ключ API
     url += text; // текст для перевода
-    url += '&lang=ru-en'; // направление перевода: с русского на английский
-
-    // Таким образом формируется строка вида:
-    // https://translate.yandex.net/api/v1.5/tr.json/translate?key=example_api_key&text=кролики&lang=ru-en
+    url += '&lang=' + origin + '-' + translation; // направление перевода: с русского на английский
 
     var translate = document.querySelector('.translate');
 
