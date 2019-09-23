745ee8a8b336722783f09780786ee2b6f91baefc
Sazuru <sazuru@gmail.com>
2019-09-23 21:57:41 +0300

    Finished task

--- Module 12/script.js
+++ Module 12/script.js
@@ -1,9 +1,26 @@
 $('.ball').on('click', function () {
-    var posY = 1 + Math.max(Math.random() * 100);
-    var posX = 1 + Math.floor(Math.random() * 1000);
-    console.log(posX, posY);
-    $('.ball').animate({
-        top: +posY,
-        left: +posX
-    }, 500);
+    var posY = 1 + Math.max(Math.random() * 500);
+    if ($('.ball').css('left') == '50px') {
+        $('.ball').animate({
+            top: +posY,
+            left: '1270px'
+        }, 500);
+        goal();
+    } else {
+        $('.ball').animate({
+            top: +posY,
+            left: '50px'
+        }, 500);
+        goal();
+    }
+    console.log(posY);
+
+    function goal() {
+        if (posY >= 290 && posY <= 410) {
+            console.log('Goal!');
+            $("h2").addClass("goal");
+        } else {
+            $("h2").removeClass("goal");
+        }
+    };
 });
