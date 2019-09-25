2daede86ca162d54ed1faa8ffe7a753c0d6f7bab
Sazuru <sazuru@gmail.com>
2019-09-25 21:07:12 +0300

    Added result table and fixed bugs

--- Module 12/script.js
+++ Module 12/script.js
@@ -1,17 +1,19 @@
 $('.ball').on('click', function () {
     var posY = 1 + Math.max(Math.random() * 500);
-    if ($('.ball').css('left') == '50px') {
+    if ($('.ball').css('left') == '0px' || $('.ball').css('left') == '50px') {
         $('.ball').animate({
-            top: posY,
+            top: 10 + posY,
             left: '1270px'
         }, 500);
         goal();
+        resultLeft();
     } else {
         $('.ball').animate({
             top: posY,
             left: '50px'
         }, 500);
         goal();
+        resultRight();
     }
     console.log(posY);
 
@@ -23,4 +25,20 @@ $('.ball').on('click', function () {
             $("h2").removeClass("goal");
         }
     };
+
+    function resultLeft() {
+        var i = $(".left").text();
+        if (posY >= 290 && posY <= 410) {
+            i++;
+            $(".left").text(i);
+        }
+    }
+
+    function resultRight() {
+        var i = $(".right").text();
+        if (posY >= 290 && posY <= 410) {
+            i++;
+            $(".right").text(i);
+        }
+    }
 });
