ce82c088546c10f743a29825c62d9e40ed0bcebd
Sazuru <sazuru@gmail.com>
2019-09-26 21:36:13 +0300

    Code refactoring

--- Module 12/script.js
+++ Module 12/script.js
@@ -1,44 +1,61 @@
-$('.ball').on('click', function () {
-    var posY = 1 + Math.max(Math.random() * 500);
-    if ($('.ball').css('left') == '0px' || $('.ball').css('left') == '50px') {
-        $('.ball').animate({
-            top: 10 + posY,
-            left: '1270px'
-        }, 500);
-        goal();
-        resultLeft();
-    } else {
-        $('.ball').animate({
-            top: posY,
-            left: '50px'
-        }, 500);
-        goal();
-        resultRight();
-    }
-    console.log(posY);
+let kick = false;
+let ballWidth = $('.ball').width();
+let ballHeight = $('.ball').height();
 
-    function goal() {
-        if (posY >= 290 && posY <= 410) {
+var posX = function () {
+    let clientWidth = $(window).width();
+    if (kick)
+        return clientWidth - ballWidth - 20;
+    else
+        return 0;
+};
+
+var posY = function () {
+    return Math.random() * ($('.field').height() - $('.ball').height());
+};
+
+$('.ball').click(function () {
+    kick = !kick;
+    var x = posX();
+    var y = posY();
+    $('.ball').animate({
+        left: x,
+        top: y
+    }, 500, function () {
+        let centerHeight = $('.field').height() / 2;
+        let minY = centerHeight - ballHeight;
+        let maxY = centerHeight + ballHeight;
+        if (y >= minY && (y + ballHeight) <= maxY) {
             console.log('Goal!');
             $("h2").addClass("goal");
+            if (x > 0) {
+                resultLeft();
+            } else {
+                resultRight();
+            }
         } else {
             $("h2").removeClass("goal");
         }
-    };
+    });
+});
 
-    function resultLeft() {
-        var i = $(".left").text();
-        if (posY >= 290 && posY <= 410) {
-            i++;
-            $(".left").text(i);
-        }
-    }
+function resultLeft() {
+    var i = $(".left").text();
+    i++;
+    $(".left").text(i);
+};
 
-    function resultRight() {
-        var i = $(".right").text();
-        if (posY >= 290 && posY <= 410) {
-            i++;
-            $(".right").text(i);
-        }
-    }
+function resultRight() {
+    var i = $(".right").text();
+    i++;
+    $(".right").text(i);
+};
+
+$(window).resize(function () {
+    if (!kick) return;
+
+    let clientWidth = $(window).width() - ballWidth - 20;
+    $('.ball').css({
+        left: clientWidth
+    });
 });
