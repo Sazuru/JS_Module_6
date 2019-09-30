8abbfc2dfd64ec9cb42461e81e57d945e96102e8
Sazuru <sazuru@gmail.com>
2019-09-30 20:35:29 +0300

    Added arrow function to added task

--- Module 13/script.js
+++ Module 13/script.js
@@ -2,13 +2,20 @@
 let myName = prompt('Как вас зовут?', 'Введите Ваше имя');
 let myLastName = prompt('Какая у вас фамилия?', 'Введите Вашу фамилию');
 let myBirthYear = prompt('Введите Ваш год рождения');
-
 let currentYear = new Date;
-let age = currentYear.getFullYear() - myBirthYear;
+let age;
 
-if (age < 20) {
+//let currentYear = new Date;
+//let age = currentYear.getFullYear() - myBirthYear;
+
+let myAge = () => {
+    let age = currentYear.getFullYear() - myBirthYear;
+    return age;
+};
+
+if (myAge() < 20) {
     alert(`Привет, ${myName} ${myLastName}!`);
-} else if (age >= 20 && age < 40) {
+} else if (myAge() >= 20 && myAge() < 40) {
     alert(`Добрый день, ${myName} ${myLastName}.`);
 } else {
     alert(`Здравствуйте, ${myName} ${myLastName}.`);
