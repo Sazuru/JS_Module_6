8c9d8b44158bf152ea5d5263cdc1cfbfeb9fa498
Valentin Ageev <sazuru@gmail.com>
2019-11-27 23:37:10 +0300

    Added some style

--- Module 15/skillbox-react/src/components/CommentsComponent.js
+++ Module 15/skillbox-react/src/components/CommentsComponent.js
@@ -1,26 +1,13 @@
 import React from "react";
+import "./CommentsComponent.css";
 
 function CommentsComponent(props) {
   return (
     <div className="App">
-      {props.data.comments.map(comment => (
-        <div key={comment.id}>
-          <span style={{ fontStyle: "italic" }}>{comment.date}:</span>
-          <p>
-            Пользователь: <strong>{comment.name},</strong>
-          </p>
-          <span>Комментарий: {comment.comment}</span>
-          <p>
-            <button onClick={props.removeComment.bind(null, comment.id)}>
-              Удалить комментарий
-            </button>
-            <hr />
-          </p>
-        </div>
-      ))}
       <div>
+        <h2>Оставьте свой комментарий</h2>
         <label>
-          Имя:
+          Имя:{" "}
           <input
             type="text"
             value={props.data.form.name}
@@ -29,15 +16,34 @@ function CommentsComponent(props) {
           ></input>
         </label>
         <label>
-          Комментарий:
+          Комментарий:{" "}
           <textarea
             name="comment"
+            placeholder="Напишите ваш комментарий"
             value={props.data.form.comment}
             onChange={props.handleChange}
-          ></textarea>
+          ></textarea>{" "}
         </label>
-        <button onClick={props.addComment}>Добавить комментарий</button>
+        <button onClick={props.addComment}> Добавить комментарий </button>
       </div>
+      <hr />
+      {props.data.comments.map(comment => (
+        <div class="comment_body">
+          <div key={comment.id}>
+            <span>{comment.date}:</span>
+            <p>
+              Пользователь: <span class="user">{comment.name}</span>,
+            </p>
+            <span>Комментарий: {comment.comment}</span>
+            <p>
+              <button onClick={props.removeComment.bind(null, comment.id)}>
+                Удалить комментарий
+              </button>
+            </p>
+          </div>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
