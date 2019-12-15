3aa5922bbf6ea85868a285db1c4bfeb4af81cd8c
Valentin Ageev <sazuru@gmail.com>
2019-12-15 17:49:00 +0300

    Created default files for task

---
+++ Module 16/skillbox-react-redux/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+import CommentsContainer from "./components/CommentsContainer";
+
+function App() {
+  return <CommentsContainer />;
+}
+export default App;
