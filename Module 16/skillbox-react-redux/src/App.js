8b48441507862528f34125ec416841cdce0af637
Valentin Ageev <sazuru@gmail.com>
2020-01-29 23:00:46 +0300

    Starting from scratch

--- Module 16/skillbox-react-redux/src/App.js
+++ Module 16/skillbox-react-redux/src/App.js
@@ -1,8 +1,111 @@
-import React from "react";
-import "./App.css";
-import CommentsContainer from "./components/CommentsContainer";
+// import React from "react";
+// import "./App.css";
+// import CommentsContainer from "./components/CommentsContainer";
 
-function App() {
-  return <CommentsContainer />;
+// function App() {
+//   return <CommentsContainer />;
+// }
+// export default App;
+
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  state = {
+    comments: [],
+    form: {
+      name: "",
+      comment: ""
+    }
+  };
+
+  componentDidMount() {
+    if (localStorage.getItem("state")) {
+      this.setState({ ...JSON.parse(localStorage.getItem("state")) });
+    }
+  }
+
+  addComment = () => {
+    this.setState(
+      {
+        comments: [
+          ...this.state.comments,
+          {
+            id: this.state.comments.length
+              ? this.state.comments.reduce((p, c) => (p.id > c.id ? p : c)).id +
+                1
+              : 1, // max id +1
+            name: this.state.form.name,
+            comment: this.state.form.comment,
+            date: new Date().toLocaleString()
+          }
+        ],
+        form: {
+          name: "",
+          comment: "",
+          date: ""
+        }
+      },
+      () => localStorage.setItem("state", JSON.stringify(this.state))
+    );
+  };
+
+  removeComment = id => {
+    this.setState(
+      {
+        comments: this.state.comments.filter(comment => comment.id !== id)
+      },
+      () => localStorage.setItem("state", JSON.stringify(this.state))
+    );
+  };
+
+  handleChange = e => {
+    console.log(e.target.name);
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.comments.map(comment => (
+          <div key={comment.id}>
+            <span style={{ fontStyle: "italic" }}>
+              {comment.id} - {comment.date}:
+            </span>
+            <strong>{comment.name}, </strong>
+            <span>{comment.comment}</span>
+            <button onClick={this.removeComment.bind(null, comment.id)}>
+              Удалить комментарий
+            </button>
+          </div>
+        ))}
+        <div>
+          <label>
+            Имя:{" "}
+            <input
+              type="text"
+              value={this.state.form.name}
+              name="name"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Комментарий:{" "}
+            <textarea
+              name="comment"
+              value={this.state.form.comment}
+              onChange={this.handleChange}
+            ></textarea>
+          </label>
+          <button onClick={this.addComment}>Добавить комментарий</button>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default App;
