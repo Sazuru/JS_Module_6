fa398ef77b26b9d134b30bb6e5bc05e497ae4c59
Valentin Ageev <sazuru@gmail.com>
2020-01-29 22:40:29 +0300

    Another try to start this module

--- Module 16/skillbox-react-redux/src/App.js
+++ Module 16/skillbox-react-redux/src/App.js
@@ -1,31 +1,8 @@
 import React from "react";
-import { connect } from "react-redux";
-import CommentList from "../src/components/CommentList";
-import AddComment from "../src/components/AddComment";
-import { addComment, removeComment } from "../src/actions";
+import "./App.css";
+import CommentsContainer from "./components/CommentsContainer";
 
-let App = ({ comments, addComment, removeComment }) => {
-  return (
-    <div>
-      <CommentList comments={comments} removeComment={removeComment} />
-      <AddComment addComment={addComment} />
-    </div>
-  );
-};
-
-const mapStateToProps = state => {
-  return {
-    comments: state
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    addComment: name => dispatch(addComment(name)),
-    removeComment: id => dispatch(removeComment(id))
-  };
-};
-
-App = connect(mapStateToProps, mapDispatchToProps)(App);
-
+function App() {
+  return <CommentsContainer />;
+}
 export default App;
--- Module 16/skillbox-react-redux/src/store/configureStore.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import comments from "../reducers";
-
-export const store = createStore(comments);
