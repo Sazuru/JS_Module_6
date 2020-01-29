6fd81deccc33b846fc4bb8d580367372e9bdae7e
Valentin Ageev <sazuru@gmail.com>
2020-01-29 20:11:11 +0300

    Suddenly refactoring

--- Module 5/lib2.js
+++ Module 5/lib2.js
@@ -1,55 +1,51 @@
-(function () {
+(function() {
+  window.start = function() {
+    var numberMain = Math.floor(Math.random() * 1000 + 1);
+    var attempt = 0;
+    var checkNumber = function() {
+      if (attempt >= 10) {
+        alert("попытки закончились!");
+        checkResume();
+        return;
+      }
 
-    window.start = function () {
+      var numberUser = prompt(
+        "угадай число! осталось " + (10 - attempt) + " попыток!"
+      );
+      if (numberUser == null) {
+        alert("GAME OVER");
+        return;
+      } else {
+        numberUser = parseInt(numberUser);
+      }
 
-        var numberMain = Math.floor((Math.random() * 1000) + 1);
-        var attempt = 0;
-        var checkNumber = function () {
-            if (attempt >= 10) {
-                alert('попытки закончились!');
-                checkResume();
-                return;
-            }
-
-            var numberUser = prompt('угадай число! осталось ' + (10 - attempt) + ' попыток!');
-            if (numberUser == null) {
-                alert('GAME OVER');
-                return;
-            } else {
-                numberUser = parseInt(numberUser);
-            };
-
-            if (isNaN(numberUser)) {
-                alert('не число!');
-                attempt += 1;
-                checkNumber();
-
-            } else if (numberUser > numberMain) {
-                alert('меньше!');
-                attempt += 1;
-                checkNumber();
-
-            } else if (numberUser < numberMain) {
-                alert('больше!');
-                attempt += 1;
-                checkNumber();
-
-            } else {
-                alert('правильно!');
-                checkResume();
-            }
-        }
-
-        var checkResume = function () {
-            var resume = confirm('сыграем еще?');
-            if (resume) {
-                start();
-
-            } else {
-                alert('GAME OVER');
-                return;
-            }
-        }
+      if (isNaN(numberUser)) {
+        alert("не число!");
+        attempt += 1;
         checkNumber();
-    }
-})()
+      } else if (numberUser > numberMain) {
+        alert("меньше!");
+        attempt += 1;
+        checkNumber();
+      } else if (numberUser < numberMain) {
+        alert("больше!");
+        attempt += 1;
+        checkNumber();
+      } else {
+        alert("правильно!");
+        checkResume();
+      }
+    };
+
+    var checkResume = function() {
+      var resume = confirm("сыграем еще?");
+      if (resume) {
+        start();
+      } else {
+        alert("GAME OVER");
+        return;
+      }
+    };
+    checkNumber();
+  };
+})();
